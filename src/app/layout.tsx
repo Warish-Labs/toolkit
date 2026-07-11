9f6e0a7d5c8823a57425761e0e9295bae09a4b57
mdwarishansari <warishansari018@proton.me>
2026-07-11 14:55:57 +0530

    feat: configure og-image metadata and add banner asset

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -56,12 +56,21 @@ export const metadata: Metadata = {
     siteName: SITE_CONFIG.name,
     locale: "en_US",
     type: "website",
+    images: [
+      {
+        url: "/og-image.png",
+        width: 1200,
+        height: 630,
+        alt: SITE_CONFIG.name,
+      },
+    ],
   },
   twitter: {
     card: "summary_large_image",
     title: SITE_CONFIG.name,
     description: SITE_CONFIG.description,
     creator: SITE_CONFIG.social.twitter,
+    images: ["/og-image.png"],
   },
   icons: {
     icon: [
